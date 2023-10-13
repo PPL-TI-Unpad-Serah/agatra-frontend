a17942eb1c26857cf511953a265d9f54cdfe8270
AncuL001 <ancul001@gmail.com>
2023-10-13 12:31:53 +0700

    add validation, fix back button

--- lib/views/login/view.dart
+++ lib/views/login/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 import 'package:go_router/go_router.dart';
 
 class LoginView extends StatelessWidget {
@@ -12,50 +13,84 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 48.0),
+      body: SafeArea(
+        child: Stack(
           children: [
-            const SizedBox(height: 32),
-            Text(
-              'Agatra',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            const SizedBox(height: 64),
-            TextFormField(
-              controller: email,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                helperText: 'eee',
-                border: OutlineInputBorder(),
+            Form(
+              key: _formKey,
+              child: ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 48.0),
+                children: [
+                  const SizedBox(height: 64.0),
+                  Text(
+                    'Agatra',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  const SizedBox(height: 64),
+                  TextFormField(
+                    controller: email,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      helperText: '',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: MultiValidator([
+                      RequiredValidator(
+                        errorText: 'Required',
+                      ),
+                    ]),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: password,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      helperText: '',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red, width: 5.0),
+                      ),
+                    ),
+                    obscureText: true,
+                    validator: MultiValidator([
+                      RequiredValidator(
+                        errorText: 'Required',
+                      ),
+                    ]),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16.0).copyWith(
+                      top: 16.0,
+                    ),
+                    child: FilledButton(
+                      child: const Text('Login'),
+                      onPressed: () {
+                        if (!_formKey.currentState!.validate()) {
+                          return;
+                        }
+                        context.pop();
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: TextButton(
+                      child: const Text('Register'),
+                      onPressed: () => context.go('/register'),
+                    ),
+                  )
+                ],
               ),
             ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: password,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                helperText: ' ',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red, width: 5.0),
-                ),
-              ),
-              obscureText: true,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FilledButton(
-                child: const Text('Login'),
+            Positioned(
+              top: 4.0,
+              left: 4.0,
+              child: IconButton(
                 onPressed: () => context.pop(),
+                icon: const Icon(Icons.arrow_back),
               ),
-            ),
-            TextButton(
-              child: const Text('Register'),
-              onPressed: () => context.go('/register'),
-            ),
+            )
           ],
         ),
       ),
--- lib/views/register/view.dart
+++ lib/views/register/view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class RegisterView extends StatelessWidget {
   RegisterView({super.key});
@@ -13,59 +14,103 @@ class RegisterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 48.0),
+      body: SafeArea(
+        child: Stack(
           children: [
-            const SizedBox(height: 32),
-            Text(
-              'Agatra',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            const SizedBox(height: 64),
-            TextFormField(
-              controller: name,
-              decoration: const InputDecoration(
-                labelText: 'Name',
-                helperText: ' ',
-                border: OutlineInputBorder(),
+            Form(
+              key: _formKey,
+              child: ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 48.0),
+                children: [
+                  const SizedBox(height: 64.0),
+                  Text(
+                    'Agatra',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  const SizedBox(height: 64),
+                  TextFormField(
+                    controller: name,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      helperText: '',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: MultiValidator([
+                      RequiredValidator(
+                        errorText: 'Required',
+                      ),
+                    ]),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: email,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      helperText: '',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: MultiValidator([
+                      RequiredValidator(
+                        errorText: 'Required',
+                      ),
+                      EmailValidator(
+                        errorText: 'Not a valid email',
+                      )
+                    ]),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: password,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      helperText: '',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                    validator: MultiValidator([
+                      RequiredValidator(
+                        errorText: 'Required',
+                      ),
+                      MinLengthValidator(
+                        8,
+                        errorText: 'Length should be greater than 8',
+                      ),
+                    ]),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16.0).copyWith(
+                      top: 16.0,
+                    ),
+                    child: FilledButton(
+                      child: const Text('Register'),
+                      onPressed: () {
+                        if (!_formKey.currentState!.validate()) {
+                          return;
+                        }
+                        context.pop();
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: TextButton(
+                      child: const Text('Login'),
+                      onPressed: () => context.go('/login'),
+                    ),
+                  )
+                ],
               ),
             ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: email,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                helperText: ' ',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextFormField(
-              controller: password,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                helperText: ' ',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red, width: 5.0),
-                ),
-              ),
-              obscureText: true,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FilledButton(
-                child: const Text('Register'),
+            Positioned(
+              top: 4.0,
+              left: 4.0,
+              child: IconButton(
                 onPressed: () => context.pop(),
+                icon: const Icon(Icons.arrow_back),
               ),
-            ),
-            TextButton(
-              child: const Text('Login'),
-              onPressed: () => context.go('/login'),
-            ),
+            )
           ],
         ),
       ),
