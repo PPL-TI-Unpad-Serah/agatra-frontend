0d677e8e5ce3acc021dcd461a3856b513974e629
AncuL001 <ancul001@gmail.com>
2023-10-14 12:03:15 +0700

    initial commit

---
+++ lib/core/resources/list_response.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'list_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ListResponse<T> _$ListResponseFromJson<T>(
+  Map<String, dynamic> json,
+  T Function(Object? json) fromJsonT,
+) =>
+    ListResponse<T>(
+      message: json['message'] as String,
+      data: (json['data'] as List<dynamic>).map(fromJsonT).toList(),
+    );
+
+Map<String, dynamic> _$ListResponseToJson<T>(
+  ListResponse<T> instance,
+  Object? Function(T value) toJsonT,
+) =>
+    <String, dynamic>{
+      'message': instance.message,
+      'data': instance.data.map(toJsonT).toList(),
+    };
