f3361d3bad5d0356c6b1c86960a58cb660791889
AncuL001 <ancul001@gmail.com>
2023-10-23 12:30:16 +0700

    use DataState

--- lib/features/domain/repository/search_arcade_locations_repository.dart
+++ lib/features/domain/repository/search_arcade_locations_repository.dart
@@ -1,11 +1,12 @@
+import 'package:agatra/core/resources/data_state.dart';
 import 'package:agatra/features/domain/entities/arcade_center.dart';
 import 'package:agatra/features/domain/entities/city.dart';
 import 'package:agatra/features/domain/entities/game_title.dart';
 import 'package:agatra/features/domain/entities/game_title_version.dart';
 
 abstract class SearchArcadeLocationsRepository {
-  Future<List<GameTitleEntity>> getGameTitles();
-  Future<List<GameTitleVersionEntity>> getGameTitleVersionsOf(GameTitleEntity gameTitle);
-  Future<List<ArcadeCenterEntity>> getArcadeCenters();
-  Future<List<CityEntity>> getCities();
+  Future<DataState<List<GameTitleEntity>>> getGameTitles();
+  Future<DataState<List<GameTitleVersionEntity>>> getGameTitleVersionsOf(GameTitleEntity gameTitle);
+  Future<DataState<List<ArcadeCenterEntity>>> getArcadeCenters();
+  Future<DataState<List<CityEntity>>> getCities();
 }
