79a9c0a7e9224d35f39589056d942ad36b7fe0ef
AncuL001 <ancul001@gmail.com>
2023-10-23 11:15:39 +0700

    add compact versions

--- lib/features/domain/entities/game_title.dart
+++ lib/features/domain/entities/game_title.dart
@@ -1,17 +1,21 @@
+import 'package:agatra/features/domain/entities/game_title_version_compact.dart';
 import 'package:equatable/equatable.dart';
 
 class GameTitleEntity extends Equatable {
   final int id;
   final String name;
+  final List<GameTitleVersionCompactEntity> versions;
 
   const GameTitleEntity({
     required this.id,
     required this.name,
+    required this.versions,
   });
 
   @override
   List<Object?> get props => [
     id,
     name,
+    versions,
   ];
 }
--- lib/features/domain/entities/game_title_version_compact.dart
+++ lib/features/domain/entities/game_title_version_compact.dart
@@ -1,16 +1,13 @@
-import 'package:agatra/features/domain/entities/game_title_compact.dart';
 import 'package:equatable/equatable.dart';
 
-class GameTitleVersionEntity extends Equatable {
+class GameTitleVersionCompactEntity extends Equatable {
   final int id;
   final String name;
-  final GameTitleCompactEntity title;
   final String info;
 
-  const GameTitleVersionEntity({
+  const GameTitleVersionCompactEntity({
     required this.id,
     required this.name,
-    required this.title,
     required this.info,
   });
   
@@ -18,7 +15,6 @@ class GameTitleVersionEntity extends Equatable {
   List<Object?> get props => [
     id,
     name,
-    title,
     info,
   ];
 }
