cba6ff4b64f43bd08d4eef6ce8bbc4033b17f3fb
AncuL001 <ancul001@gmail.com>
2023-10-23 19:13:41 +0700

    use new riverpod syntax

---
+++ lib/providers.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dioHash() => r'58eeefbd0832498ca2574c1fe69ed783c58d1d8f';
+
+/// See also [dio].
+@ProviderFor(dio)
+final dioProvider = AutoDisposeProvider<Dio>.internal(
+  dio,
+  name: r'dioProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$dioHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef DioRef = AutoDisposeProviderRef<Dio>;
+String _$apiServiceHash() => r'c2e6326396cebbc4d74ee037b494872ee4f830db';
+
+/// See also [apiService].
+@ProviderFor(apiService)
+final apiServiceProvider = AutoDisposeProvider<ApiService>.internal(
+  apiService,
+  name: r'apiServiceProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$apiServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ApiServiceRef = AutoDisposeProviderRef<ApiService>;
+String _$searchArcadeLocationsRepositoryHash() =>
+    r'dd3a1a23fab108c07739fbb754e27a8b587668ad';
+
+/// See also [searchArcadeLocationsRepository].
+@ProviderFor(searchArcadeLocationsRepository)
+final searchArcadeLocationsRepositoryProvider =
+    AutoDisposeProvider<SearchArcadeLocationsRepository>.internal(
+  searchArcadeLocationsRepository,
+  name: r'searchArcadeLocationsRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$searchArcadeLocationsRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SearchArcadeLocationsRepositoryRef
+    = AutoDisposeProviderRef<SearchArcadeLocationsRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
