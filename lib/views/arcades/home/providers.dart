3bd292df96615cf72a092da73771d24a66db189f
AncuL001 <ancul001@gmail.com>
2023-11-04 10:15:40 +0700

    create getCities futureProvider

---
+++ lib/views/arcades/home/providers.dart
@@ -0,0 +1,20 @@
+import 'package:agatra/core/resources/data_state.dart';
+import 'package:agatra/features/domain/entities/city.dart';
+import 'package:agatra/providers.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'providers.g.dart';
+
+@riverpod
+Future<List<CityEntity>> getCities(GetCitiesRef ref) async {
+  final searchArcadeLocationRepository = ref.watch(searchArcadeLocationsRepositoryProvider); 
+
+  final result = await searchArcadeLocationRepository.getCities();
+
+  if (result is DataFailure) {
+    return Future.error(result.error!);
+  } else {
+    return Future.value(result.data!);
+  }
+}
+
