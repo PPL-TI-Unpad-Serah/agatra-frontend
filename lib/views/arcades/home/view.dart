996313b64ebcf3f936d7c275f159e17eb071c96e
AncuL001 <ancul001@gmail.com>
2023-11-05 10:18:54 +0700

    use entities on components

--- lib/views/arcades/home/view.dart
+++ lib/views/arcades/home/view.dart
@@ -1,3 +1,6 @@
+import 'package:agatra/features/domain/entities/arcade_location_compact.dart';
+import 'package:agatra/features/domain/entities/game_title_compact.dart';
+import 'package:agatra/features/domain/entities/game_title_version.dart';
 import 'package:agatra/views/arcades/home/search_bottom_sheet.dart';
 import 'package:agatra/views/arcades/home/search_query_provider.dart';
 import 'package:flutter/material.dart';
@@ -16,17 +19,38 @@ class ArcadesSearchView extends ConsumerWidget {
       appBar: AppBar(),
       body: ListView(
         padding: const EdgeInsets.all(8.0),
-        children: [for (int i = 0; i < 12; i++) _ArcadeItemCard()],
+        children: [
+          for (int i = 0; i < 12; i++)
+            _ArcadeItemCard(
+              location: ArcadeLocationCompactEntity(
+                id: i,
+                name: "Arcade $i",
+                games: [
+                  for (int j = 0; j < 5; j++)
+                    GameTitleVersionEntity(
+                      id: i,
+                      name: "Game $j",
+                      info: "Info $j",
+                      title: GameTitleCompactEntity(
+                        id: j,
+                        name: "Title $j",
+                      ),
+                    ),
+                ],
+              ),
+            )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet<void>(
-              context: context,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              constraints: const BoxConstraints(maxWidth: 640),
-              builder: (context) => const SearchBottomSheetList());
+            context: context,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            constraints: const BoxConstraints(maxWidth: 640),
+            builder: (context) => const SearchBottomSheetList(),
+          );
         },
         child: const Icon(Icons.search),
       ),
@@ -35,6 +59,13 @@ class ArcadesSearchView extends ConsumerWidget {
 }
 
 class _ArcadeItemCard extends StatelessWidget {
+  final ArcadeLocationCompactEntity location;
+
+  const _ArcadeItemCard({
+    super.key,
+    required this.location,
+  });
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -46,31 +77,18 @@ class _ArcadeItemCard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Timezone Bandung Indah Plaza",
+              Text(location.name,
                   style: Theme.of(context).textTheme.titleLarge),
               const SizedBox(height: 16.0),
-              const SizedBox(
+              SizedBox(
                 width: double.infinity,
                 child: Wrap(
                   alignment: WrapAlignment.spaceAround,
                   spacing: 8.0,
                   runSpacing: 12.0,
                   children: [
-                    _ArcadeChipWidget(
-                      name: 'maimai FiNALE',
-                    ),
-                    _ArcadeChipWidget(
-                      name: 'Pump It Up XX',
-                    ),
-                    _ArcadeChipWidget(
-                      name: 'SOUND VOLTEX EXCEED GEAR',
-                    ),
-                    _ArcadeChipWidget(
-                      name: 'Danz Base',
-                    ),
-                    _ArcadeChipWidget(
-                      name: 'Wangan Midnight Maximum Tune 6RR',
-                    ),
+                    for (final version in location.games)
+                      _ArcadeChipWidget(item: version)
                   ],
                 ),
               )
@@ -83,11 +101,11 @@ class _ArcadeItemCard extends StatelessWidget {
 }
 
 class _ArcadeChipWidget extends StatelessWidget {
-  final String name;
+  final GameTitleVersionEntity item;
 
   const _ArcadeChipWidget({
     super.key,
-    required this.name,
+    required this.item,
   });
 
   @override
@@ -100,7 +118,7 @@ class _ArcadeChipWidget extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
         child: Text(
-          name,
+          "${item.name} ${item.title.name}",
           style: TextStyle(
             height: 1.2,
             color: Theme.of(context)
