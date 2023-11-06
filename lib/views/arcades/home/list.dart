745514aee7d81b7704f5d36e6d4f04ea6525420c
AncuL001 <ancul001@gmail.com>
2023-11-06 07:36:33 +0700

    move list into separate class

---
+++ lib/views/arcades/home/list.dart
@@ -0,0 +1,114 @@
+import 'package:agatra/features/domain/entities/arcade_location_compact.dart';
+import 'package:agatra/features/domain/entities/game_title_compact.dart';
+import 'package:agatra/features/domain/entities/game_title_version.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class ArcadeLocationsListView extends ConsumerWidget {
+  const ArcadeLocationsListView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListView(
+      padding: const EdgeInsets.all(8.0),
+      children: [
+        for (int i = 0; i < 12; i++)
+          _ArcadeItemCard(
+            location: ArcadeLocationCompactEntity(
+              id: i,
+              name: "Arcade $i",
+              games: [
+                for (int j = 0; j < 5; j++)
+                  GameTitleVersionEntity(
+                    id: i,
+                    name: "Game $j",
+                    info: "Info $j",
+                    title: GameTitleCompactEntity(
+                      id: j,
+                      name: "Title $j",
+                    ),
+                  ),
+              ],
+            ),
+          )
+      ],
+    );
+  }
+}
+
+class _ArcadeItemCard extends StatelessWidget {
+  final ArcadeLocationCompactEntity location;
+
+  const _ArcadeItemCard({
+    super.key,
+    required this.location,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => context.go('/arcades/1'),
+      child: Card(
+        elevation: 1,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(location.name,
+                  style: Theme.of(context).textTheme.titleLarge),
+              const SizedBox(height: 16.0),
+              SizedBox(
+                width: double.infinity,
+                child: Wrap(
+                  alignment: WrapAlignment.spaceAround,
+                  spacing: 8.0,
+                  runSpacing: 12.0,
+                  children: [
+                    for (final version in location.games)
+                      _ArcadeChipWidget(item: version)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ArcadeChipWidget extends StatelessWidget {
+  final GameTitleVersionEntity item;
+
+  const _ArcadeChipWidget({
+    super.key,
+    required this.item,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.tertiaryContainer.withAlpha(200),
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
+        child: Text(
+          "${item.name} ${item.title.name}",
+          style: TextStyle(
+            height: 1.2,
+            color: Theme.of(context)
+                .colorScheme
+                .onTertiaryContainer
+                .withAlpha(200),
+            fontWeight: FontWeight.w500,
+            fontSize: Theme.of(context).textTheme.bodySmall!.fontSize!,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/arcades/home/view.dart
+++ lib/views/arcades/home/view.dart
@@ -1,11 +1,8 @@
-import 'package:agatra/features/domain/entities/arcade_location_compact.dart';
-import 'package:agatra/features/domain/entities/game_title_compact.dart';
-import 'package:agatra/features/domain/entities/game_title_version.dart';
+import 'package:agatra/views/arcades/home/list.dart';
 import 'package:agatra/views/arcades/home/search_bottom_sheet.dart';
 import 'package:agatra/views/arcades/home/search_query_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class ArcadesSearchView extends ConsumerWidget {
   const ArcadesSearchView({super.key});
@@ -17,30 +14,7 @@ class ArcadesSearchView extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
-        children: [
-          for (int i = 0; i < 12; i++)
-            _ArcadeItemCard(
-              location: ArcadeLocationCompactEntity(
-                id: i,
-                name: "Arcade $i",
-                games: [
-                  for (int j = 0; j < 5; j++)
-                    GameTitleVersionEntity(
-                      id: i,
-                      name: "Game $j",
-                      info: "Info $j",
-                      title: GameTitleCompactEntity(
-                        id: j,
-                        name: "Title $j",
-                      ),
-                    ),
-                ],
-              ),
-            )
-        ],
-      ),
+      body: const ArcadeLocationsListView(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet<void>(
@@ -53,82 +27,6 @@ class ArcadesSearchView extends ConsumerWidget {
           );
         },
         child: const Icon(Icons.search),
-      ),
-    );
-  }
-}
-
-class _ArcadeItemCard extends StatelessWidget {
-  final ArcadeLocationCompactEntity location;
-
-  const _ArcadeItemCard({
-    super.key,
-    required this.location,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => context.go('/arcades/1'),
-      child: Card(
-        elevation: 1,
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(location.name,
-                  style: Theme.of(context).textTheme.titleLarge),
-              const SizedBox(height: 16.0),
-              SizedBox(
-                width: double.infinity,
-                child: Wrap(
-                  alignment: WrapAlignment.spaceAround,
-                  spacing: 8.0,
-                  runSpacing: 12.0,
-                  children: [
-                    for (final version in location.games)
-                      _ArcadeChipWidget(item: version)
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _ArcadeChipWidget extends StatelessWidget {
-  final GameTitleVersionEntity item;
-
-  const _ArcadeChipWidget({
-    super.key,
-    required this.item,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.tertiaryContainer.withAlpha(200),
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
-        child: Text(
-          "${item.name} ${item.title.name}",
-          style: TextStyle(
-            height: 1.2,
-            color: Theme.of(context)
-                .colorScheme
-                .onTertiaryContainer
-                .withAlpha(200),
-            fontWeight: FontWeight.w500,
-            fontSize: Theme.of(context).textTheme.bodySmall!.fontSize!,
-          ),
-        ),
       ),
     );
   }
