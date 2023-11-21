60a501532cd1e8a1f2ca2c33bd1165b267af4e61
AncuL001 <ancul001@gmail.com>
2023-11-21 14:42:41 +0700

    create models

---
+++ lib/features/data/models/arcade_location.dart
@@ -0,0 +1,29 @@
+import 'package:agatra/features/data/models/arcade_center.dart';
+import 'package:agatra/features/data/models/arcade_machine.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'arcade_location.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class ArcadeLocationModel {
+  final int id;
+  final String name;
+  final ArcadeCenterModel arcadeCenter;
+  final String description;
+  final double lat;
+  final double long;
+  final List<ArcadeMachineModel> arcadeMachines;
+
+  const ArcadeLocationModel({
+    required this.id,
+    required this.name,
+    required this.arcadeCenter,
+    required this.description,
+    required this.lat,
+    required this.long,
+    required this.arcadeMachines,
+  });
+
+  factory ArcadeLocationModel.fromJson(Map<String, dynamic> json) => _$ArcadeLocationModelFromJson(json);
+  Map<String, dynamic> toJson() => _$ArcadeLocationModelToJson(this);
+}
---
+++ lib/features/data/models/arcade_machine.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'arcade_machine.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ArcadeMachineModel _$ArcadeMachineModelFromJson(Map<String, dynamic> json) =>
+    ArcadeMachineModel(
+      id: json['id'] as int,
+      version: GameTitleVersionModel.fromJson(
+          json['version'] as Map<String, dynamic>),
+      machineCount: json['machine_count'] as int,
+      price: json['price'] as int,
+    );
+
+Map<String, dynamic> _$ArcadeMachineModelToJson(ArcadeMachineModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'version': instance.version,
+      'machine_count': instance.machineCount,
+      'price': instance.price,
+    };
