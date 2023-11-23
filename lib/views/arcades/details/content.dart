5f64bc9232945944273939d43c59d77b7794d0d9
AncuL001 <ancul001@gmail.com>
2023-11-24 02:28:39 +0700

    add maintainer-exclusive buttons, remove old code

--- lib/views/arcades/details/content.dart
+++ lib/views/arcades/details/content.dart
@@ -20,11 +20,12 @@ class _ArcadeLocationContent extends ConsumerWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                ElevatedButton(
+                FilledButton.icon(
                   onPressed: () {
                     context.push('/maintainer/edit-location/5');
                   },
-                  child: const Icon(Icons.edit),
+                  icon: const Icon(Icons.edit),
+                  label: const Text('Location'),
                 ),
               ],
             ),
@@ -67,210 +68,32 @@ class _ArcadeLocationContent extends ConsumerWidget {
             endIndent: 12,
           ),
           const SizedBox(height: 8.0),
-          Text(
-            'Games',
-            style: Theme.of(context).textTheme.titleLarge,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Games',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              if (session != null && session.user.role != UserRole.user) ...[
+                const Spacer(),
+                SizedBox(
+                  height: 30,
+                  child: FilledButton(
+                    onPressed: () {
+                      context.push('/maintainer/edit-location/5');
+                    },
+                    child: const Icon(Icons.add)
+                  ),
+                ),
+              ],
+            ],
           ),
           const SizedBox(height: 8.0),
           for (final machine in location.machines) ...[
             _MachineCard(machine: machine),
             const SizedBox(height: 4.0),
           ],
-          // Card(
-          //   elevation: 1,
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(16.0),
-          //     child: Column(
-          //       crossAxisAlignment: CrossAxisAlignment.start,
-          //       children: [
-          //         Text(
-          //           'maimai FiNALE',
-          //           style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          //                 color: Theme.of(context).colorScheme.secondary,
-          //               ),
-          //         ),
-          //         const SizedBox(height: 4.0),
-          //         const Padding(
-          //           padding: EdgeInsets.only(left: 16.0),
-          //           child: Column(
-          //             crossAxisAlignment: CrossAxisAlignment.start,
-          //             children: [
-          //               Text(
-          //                 'Rp10000/Credit',
-          //               ),
-          //               Text(
-          //                 'Note: 1 Credit = 4 Songs',
-          //               ),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-          // const SizedBox(height: 4.0),
-          // Card(
-          //   elevation: 1,
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(16.0),
-          //     child: Column(
-          //       crossAxisAlignment: CrossAxisAlignment.start,
-          //       children: [
-          //         Wrap(
-          //           crossAxisAlignment: WrapCrossAlignment.center,
-          //           children: [
-          //             Text(
-          //               'SOUND VOLTEX EXCEED GEAR',
-          //               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          //                     color: Theme.of(context).colorScheme.secondary,
-          //                   ),
-          //             ),
-          //             const SizedBox(width: 4.0),
-          //             const Text("x2"),
-          //             GestureDetector(
-          //               onTap: () {
-          //                 showDialog(
-          //                   context: context,
-          //                   builder: (context) => const AlertDialog(
-          //                     insetPadding: EdgeInsets.all(16),
-          //                     content: Text(
-          //                       "LIGHT START: 5 Credit\n"
-          //                       "STANDARD START: 6 Credits\n"
-          //                       "AUTOMATION START: 7 Credits\n"
-          //                       "BLASTER START: 10 Credits",
-          //                     ),
-          //                   ),
-          //                 );
-          //               },
-          //               child: const Padding(
-          //                 padding: EdgeInsets.symmetric(horizontal: 5.0),
-          //                 child: Icon(Icons.info_outline, size: 16),
-          //               ),
-          //             ),
-          //           ],
-          //         ),
-          //         const SizedBox(height: 4.0),
-          //         const Padding(
-          //           padding: EdgeInsets.only(left: 16.0),
-          //           child: Column(
-          //             crossAxisAlignment: CrossAxisAlignment.start,
-          //             children: [
-          //               Text(
-          //                 'Rp1900 / Credit',
-          //               ),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-          // const SizedBox(height: 4.0),
-          // Card(
-          //   elevation: 1,
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(16.0),
-          //     child: Column(
-          //       crossAxisAlignment: CrossAxisAlignment.start,
-          //       children: [
-          //         Text(
-          //           'Pump It Up Phoenix',
-          //           style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          //                 color: Theme.of(context).colorScheme.secondary,
-          //               ),
-          //         ),
-          //         const SizedBox(height: 4.0),
-          //         const Padding(
-          //           padding: EdgeInsets.only(left: 16.0),
-          //           child: Column(
-          //             crossAxisAlignment: CrossAxisAlignment.start,
-          //             children: [
-          //               Text(
-          //                 'Rp10000/Credit',
-          //               ),
-          //               Text(
-          //                 'Note: 1 Credit = 4 Songs',
-          //               ),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-          // const SizedBox(height: 4.0),
-          // Card(
-          //   elevation: 1,
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(16.0),
-          //     child: Column(
-          //       crossAxisAlignment: CrossAxisAlignment.start,
-          //       children: [
-          //         Text(
-          //           'Danz Base',
-          //           style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          //                 color: Theme.of(context).colorScheme.secondary,
-          //               ),
-          //         ),
-          //         const SizedBox(height: 4.0),
-          //         const Padding(
-          //           padding: EdgeInsets.only(left: 16.0),
-          //           child: Column(
-          //             crossAxisAlignment: CrossAxisAlignment.start,
-          //             children: [
-          //               Text(
-          //                 'Rp10000/Credit',
-          //               ),
-          //               Text(
-          //                 'Note: 1 Credit = 4 Songs',
-          //               ),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-          // const SizedBox(height: 4.0),
-          // Card(
-          //   elevation: 1,
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(16.0),
-          //     child: Column(
-          //       crossAxisAlignment: CrossAxisAlignment.start,
-          //       children: [
-          //         Wrap(
-          //           crossAxisAlignment: WrapCrossAlignment.center,
-          //           children: [
-          //             Text(
-          //               'Wangan Midnight Maximum Tune 6RR',
-          //               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          //                     color: Theme.of(context).colorScheme.secondary,
-          //                   ),
-          //             ),
-          //             const SizedBox(width: 4.0),
-          //             const Text("x4"),
-          //           ],
-          //         ),
-          //         const SizedBox(height: 4.0),
-          //         const Padding(
-          //           padding: EdgeInsets.only(left: 16.0),
-          //           child: Column(
-          //             crossAxisAlignment: CrossAxisAlignment.start,
-          //             children: [
-          //               Text(
-          //                 'Rp10000/Credit',
-          //               ),
-          //               Text(
-          //                 'Note: 1 Credit = 4 Songs',
-          //               ),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
         ],
       );
   }
