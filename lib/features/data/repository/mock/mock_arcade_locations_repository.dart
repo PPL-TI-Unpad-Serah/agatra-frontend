8e882118c144b93535d51d7f24d55bd75df6fa14
AncuL001 <ancul001@gmail.com>
2023-11-25 19:15:41 +0700

    create new repository methods

--- lib/features/data/repository/mock/mock_arcade_locations_repository.dart
+++ lib/features/data/repository/mock/mock_arcade_locations_repository.dart
@@ -5,6 +5,7 @@ import 'package:agatra/features/data/data_mapper.dart';
 import 'package:agatra/features/data/models/arcade_location.dart';
 import 'package:agatra/features/domain/entities/arcade_location.dart';
 import 'package:agatra/features/domain/entities/arcade_machine.dart';
+import 'package:agatra/features/domain/entities/form/new_arcade_location.dart';
 import 'package:agatra/features/domain/repository/arcade_locations_repository.dart';
 import 'package:flutter/services.dart';
 
@@ -41,7 +42,22 @@ class MockArcadeLocationsRepository extends ArcadeLocationsRepository {
     return const DataSuccess(null);
   }
 
+  @override
+  Future<DataState> createArcadeLocation(NewArcadeLocationEntity body) async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+
+    return const DataSuccess(null);
+  }
+
+  @override
+  Future<DataState> deleteArcadeMachine(ArcadeMachineEntity body) async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+
+    return const DataSuccess(null);
+  }
+
   Future<String> _loadAsset(String path) async {
     return rootBundle.loadString(path);
   }
+
 }
--- lib/features/domain/repository/arcade_locations_repository.dart
+++ lib/features/domain/repository/arcade_locations_repository.dart
@@ -1,10 +1,13 @@
 import 'package:agatra/core/resources/data_state.dart';
 import 'package:agatra/features/domain/entities/arcade_location.dart';
+import 'package:agatra/features/domain/entities/form/new_arcade_location.dart';
 import 'package:agatra/features/domain/entities/arcade_machine.dart';
 
 abstract class ArcadeLocationsRepository {
+  Future<DataState> createArcadeLocation(NewArcadeLocationEntity body);
   Future<DataState> updateArcadeLocation(ArcadeLocationEntity body);
 
   Future<DataState<ArcadeMachineEntity>> getArcadeMachine(int id);
   Future<DataState> updateArcadeMachine(ArcadeMachineEntity body);
+  Future<DataState> deleteArcadeMachine(ArcadeMachineEntity body);
 }
